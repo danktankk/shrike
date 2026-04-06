8b6848abfd604b4657c5ff1e24321839cdc7ba02
Praxis <praxxster@gmail.com>
2026-04-06 12:17:48 +0000

    fix: put_config State extractor, static_handler Uri param

--- src/api/notifications.rs
+++ src/api/notifications.rs
@@ -3,5 +3,5 @@ use axum::{extract::{Path, State}, http::StatusCode, response::IntoResponse};
 use super::AppState;
 
 pub async fn get_config(State(_s): State<AppState>) -> impl IntoResponse { StatusCode::NOT_IMPLEMENTED }
-pub async fn put_config() -> impl IntoResponse { StatusCode::NOT_IMPLEMENTED }
+pub async fn put_config(State(_s): State<AppState>) -> impl IntoResponse { StatusCode::NOT_IMPLEMENTED }
 pub async fn test_channel(State(_s): State<AppState>, Path(_channel): Path<String>) -> impl IntoResponse { StatusCode::NOT_IMPLEMENTED }
--- src/assets.rs
+++ src/assets.rs
@@ -1,6 +1,7 @@
 // src/assets.rs
+use axum::http::Uri;
 use axum::{http::StatusCode, response::IntoResponse};
 
-pub async fn static_handler() -> impl IntoResponse {
+pub async fn static_handler(_uri: Uri) -> impl IntoResponse {
     StatusCode::NOT_FOUND
 }
